62ffaadb3411444fac0a9593e2635f95d8006d3b
prachi9124 <prachianad321@gmail.com>
2023-11-05 21:31:02 +0530

    Playing With Characters Code Done

---
+++ C/playingwithchars.c
@@ -0,0 +1,23 @@
+//Playing With Characters
+
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() 
+{
+
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */  
+    char ch;
+    char s[100];
+    char sen[1000];
+    scanf("%c", &ch);
+    scanf("%s",s);
+    scanf("\n");
+    scanf("%[^\n]%*c",sen);
+    printf("%c \n",ch);
+    printf("%s \n", s);
+    printf("%s \n", sen);
+    return 0;
+}
