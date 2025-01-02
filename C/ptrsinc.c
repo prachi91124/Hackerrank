f45c447d28a0bac55818bf1c5e39c2adb1d1403f
Prachi Anand <prachianand321@gmail.com>
2025-01-03 00:59:49 +0530

    Pointers in C of C done

---
+++ C/ptrsinc.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <math.h>
+
+void update(int *a, int *b)
+{
+    // Complete this function
+    int sum = *a + *b;
+    int diff = *a - *b;
+
+    if (diff < 0)
+    {
+        diff = diff * -1;
+    }
+
+    *a = sum;
+    *b = diff;
+}
+
+int main()
+{
+    int a, b;
+    int *pa = &a, *pb = &b;
+
+    scanf("%d %d", &a, &b);
+    update(pa, pb);
+    printf("%d\n%d", a, b);
+
+    return 0;
+}
