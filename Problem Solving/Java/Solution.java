71d9e38e81e1351e408fdae31d17e180dfe54dd3
Prachi Anand <prachianand321@gmail.com>
2025-02-05 21:20:53 +0530

    Birthday Cake Candles of problem solving solved in Java

---
+++ Problem Solving/Java/Solution.java
@@ -0,0 +1,58 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+class Result {
+
+    /*
+     * Complete the 'birthdayCakeCandles' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts INTEGER_ARRAY candles as parameter.
+     */
+
+    public static int birthdayCakeCandles(List<Integer> candles) {
+        // Write your code here
+        HashMap<Integer, Integer> map = new HashMap<>();
+        int mx = Collections.max(candles);
+        for (int i = 0; i < candles.size(); i++) {
+            if (map.containsKey(candles.get(i))) {
+                map.put(candles.get(i), map.get(candles.get(i)) + 1);
+            } else {
+                map.put(candles.get(i), 1);
+            }
+        }
+        return map.get(mx);
+    }
+
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int candlesCount = Integer.parseInt(bufferedReader.readLine().trim());
+
+        String[] candlesTemp = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+
+        List<Integer> candles = new ArrayList<>();
+
+        for (int i = 0; i < candlesCount; i++) {
+            int candlesItem = Integer.parseInt(candlesTemp[i]);
+            candles.add(candlesItem);
+        }
+
+        int result = Result.birthdayCakeCandles(candles);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
